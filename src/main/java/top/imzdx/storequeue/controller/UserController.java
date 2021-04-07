6f0142ca81d03499d389eb2cc78f3edcc928a275
liusiming <1760602325@qq.com>
2021-04-07 19:23:51 +0800

    刘思铭的第一个测试

--- src/main/java/top/imzdx/storequeue/controller/UserController.java
+++ src/main/java/top/imzdx/storequeue/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
         User user = userService.login(uname, password);
         HttpSession session = request.getSession();
         session.setAttribute("user", user);
-
+//刘思铭
         if (user != null) {
             User userinfo = user;
             userinfo.setPassword("***");
