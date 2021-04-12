df6afd884f59bd92c7e8de5ed3986c61a469fc09
Renxing <1277489864@qq.com>
2021-04-12 15:33:14 +0800

    规整命名规范

--- src/main/java/top/imzdx/storequeue/dao/GoodsDao.java
+++ src/main/java/top/imzdx/storequeue/dao/GoodsDao.java
@@ -18,8 +18,8 @@ public interface GoodsDao {
     @Select("SELECT * FROM `category`")
     List<Category> getCategory();
 
-    @Select("select * from goods where category =#{category} or #{category} is null")
-    List<Goods> getGoodsBycategoory(@Param("category")String category);
+    @Select("select * from goods where category =#{category} or #{category} is null or 0=#{category}")
+    List<Goods> getGoodsByCategory(@Param("category") String category);
 
 
 }
