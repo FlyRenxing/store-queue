370825cf45b3a6f62a336329c0fda16b600c6908
Renxing <1277489864@qq.com>
2021-04-17 20:57:34 +0800

    新增仪表盘信息

--- src/main/java/top/imzdx/storequeue/controller/AdminController.java
+++ src/main/java/top/imzdx/storequeue/controller/AdminController.java
@@ -1,9 +1,12 @@
 package top.imzdx.storequeue.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.imzdx.storequeue.dao.AdminDao;
 import top.imzdx.storequeue.interceptor.AdminRequired;
+import top.imzdx.storequeue.pojo.Dashboard;
 import top.imzdx.storequeue.result.Result;
 import top.imzdx.storequeue.result.ResultTools;
 
@@ -15,10 +18,19 @@ import top.imzdx.storequeue.result.ResultTools;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private AdminDao adminDao;
+
     @GetMapping("dashboard")
     @AdminRequired
     public Result getInfo() {
-        return new ResultTools().success("获取成功", null);
+        Dashboard dashboard = new Dashboard();
+        dashboard.setGvm(adminDao.getGvm());
+        dashboard.setGoodsNum(adminDao.getGoodNum());
+        dashboard.setUserNum(adminDao.getUserNum());
+        dashboard.setOrderNum(adminDao.getOrderNum());
+
+        return new ResultTools().success("获取成功", dashboard);
     }
 
 }
--- src/main/java/top/imzdx/storequeue/controller/OrderController.java
+++ src/main/java/top/imzdx/storequeue/controller/OrderController.java
@@ -1,21 +1,14 @@
 package top.imzdx.storequeue.controller;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import top.imzdx.storequeue.interceptor.AdminRequired;
-import top.imzdx.storequeue.interceptor.LoginRequired;
+import top.imzdx.storequeue.service.OrderService;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-    //对订单的增删改查
-    @PostMapping("add")
-    @LoginRequired
-    public void addOrder(String adid,String gid){//
-
-    }
+    @Autowired
+    private OrderService orderService;
 
 }
