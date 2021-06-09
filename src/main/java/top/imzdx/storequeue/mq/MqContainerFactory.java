67fb7639f46dd010c53190fb7e023e5f87ce299f
Renxing <1277489864@qq.com>
2021-06-09 10:26:53 +0800

    redission的config使用配置文件内的配置

--- src/main/java/top/imzdx/storequeue/mq/MqContainerFactory.java
+++ src/main/java/top/imzdx/storequeue/mq/MqContainerFactory.java
@@ -2,14 +2,13 @@ package top.imzdx.storequeue.mq;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import top.imzdx.flashmq.JmsConnectionFactory;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSContext;
-import java.io.IOException;
 
 /**
  * @author Renxing
@@ -19,23 +18,18 @@ import java.io.IOException;
 @Configuration
 public class MqContainerFactory {
 
-//    @Bean
-//    public JmsListenerContainerFactory<?> topicListenerContainer(ConnectionFactory activeMQConnectionFactory) {
-//        DefaultJmsListenerContainerFactory topicListenerContainer = new DefaultJmsListenerContainerFactory();
-//        topicListenerContainer.setPubSubDomain(true);
-//        topicListenerContainer.setConnectionFactory(activeMQConnectionFactory);
-//        return topicListenerContainer;
-//    }
+    @Autowired
+    private RedissonClient redissonClient;
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        Config config = null;
-        try {
-            config = Config.fromYAML(MqContainerFactory.class.getClassLoader().getResource("redisson-config.yml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        RedissonClient client = Redisson.create(config);
+//        Config config = null;
+//        try {
+//            config = Config.fromYAML(MqContainerFactory.class.getClassLoader().getResource("redisson-config.yml"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        RedissonClient client = Redisson.create(redissonClient.getConfig());
         ConnectionFactory connectionFactory = new JmsConnectionFactory("store", client);
         return connectionFactory;
     }
